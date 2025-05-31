a32c1caa6a5111e80790173157aa5864acb81126
Kan-A-Pesh <michelm.gaming@gmail.com>
2025-05-31 19:11:01 +0200

    feat: create migration to drop 'interested_program' and 'gdpr_consent' columns from contact_request table

---
+++ back/migrations/Version20250531164427.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250531164427 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE contact_request DROP interested_program
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE contact_request DROP gdpr_consent
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE contact_request ADD interested_program VARCHAR(100) NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE contact_request ADD gdpr_consent BOOLEAN NOT NULL
+        SQL);
+    }
+}
